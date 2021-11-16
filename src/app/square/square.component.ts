8a9a8e6a65e9355e422fbcbd08425644d15f5eba
Roberto <r.attanasio90@gmail.com>
2021-11-16 09:38:27 +0100

    Refactor di squareComponent

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
   styleUrls: ['./square.component.css'],
 })
-export class SquareComponent {}
+export class SquareComponent {
+  @Input() answer: string;
+  @Input() tableLocked: boolean;
+}
